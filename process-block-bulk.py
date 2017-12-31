b986e8acbc1286dd3bf8c34af24f9622c43dc2c4
Saurabh Bathe <sbathe@hortonworks.com>
2017-12-31 19:26:33 +0530

    some changes

--- process-block-bulk.py
+++ process-block-bulk.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python
-bulk = open('/home/sbathe/Downloads/bulk/03-09-2007-TO-16-09-2015_bulk.csv','r').readlines()
-block = open('/home/sbathe/Downloads/bulk/03-09-2007-TO-18-09-2015_block.csv','r').readlines()
+# Bulk deals
+#Curl 'https://www.nseindia.com/products/dynaContent/equities/equities/bulkdeals.jsp?symbol=&segmentLink=13&symbolCount=&dateRange=24month&fromDate=&toDate=&dataType=DEALS' -H 'Cookie: ext_name=jaehkpjddfdgiiefcnhahapilbejohhj' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36' -H 'Accept: */*' -H 'Referer: https://www.nseindia.com/products/content/equities/equities/bulk.htm' -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' --compressed ;
+# Block Deals
+#curl 'https://www.nseindia.com/products/dynaContent/equities/equities/blockdeals.jsp?symbol=&segmentLink=12&symbolCount=&dateRange=24month&fromDate=&toDate=&dataType=BLOCK' -H 'Cookie: ext_name=jaehkpjddfdgiiefcnhahapilbejohhj' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36' -H 'Accept: */*' -H 'Referer: https://www.nseindia.com/products/content/equities/equities/bulk.htm' -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' --compressed
+
+#soup = BeautifulSoup(open("test.html"))
+#table = soup.select_one("table")
+#headers = [th.text.encode("utf-8") for th in table.select("tr th")]
+#rows = [[td.text.encode("utf-8") for td in row.find_all("td")] for row in table.select("tr + tr")]
+import requests
+
+#bulk = open('2yrsbulk.csv').readlines()
+#block = open('2yrsblock.csv').readlines()
+def get_csv_content(url):
+    r = requests.get(url)
+    if r.status_code == 200:
+        return r.text.splitlines()
 
 #investors = [ "Ramdev", "Raamdeo", "Jhunhunwala", "Rare Enterprises", "Sajay Bakshi", "ValueQuest", "Nirmal Jain", "Indiainfoline", "India infoline", "Kenneth Andrade", "Damani", "Dharamshi", "ValueQuest", "Brightstar", "Nirmal Bang", "Porinju Veliyath", "Nilesh Shah", "Ekansh Mittal", "Mittal Consulting", "Vijay Kedia", "Daljeet Kohli", "Dolly", "rajiv khanna", "Nalanda", "Indianivesh", "india nivesh" ]
 
-investors = [ "Ramdev", "Raamdeo", "Jhunhunwala", "Rare Enterprises", "Sajay Bakshi", "ValueQuest", "Damani", "Dharamshi", "ValueQuest", "Brightstar", "Veliyath", "Nalanda", "Indianivesh", "india nivesh" ]
+investors = [ "Ramdev", "Raamdeo", "Jhunhunwala", "Rare Enterprises", "Sajay Bakshi", "ValueQuest", "Damani", "Dharamshi", "ValueQuest", "Brightstar", "Veliyath", "Nalanda", "Indianivesh", "india nivesh", "Ekansh", "Katalyst Wealth" ]
 
+bulk = get_csv_content('https://nseindia.com/content/equities/bulk.csv')
+block = get_csv_content('https://nseindia.com/content/equities/block.csv')
 bulkblock = bulk + block
 
 op = ''
@@ -14,4 +31,4 @@ for i in investors:
         if i.lower() in line.lower():
             op = op + line
 
-open('/home/sbathe/Downloads/bulk/03-09-2007-TO-18-09-2015_flags.csv','w').write(op)
+open('flags.csv','a').write(op)
