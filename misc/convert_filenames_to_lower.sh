833625d0e4e1fd4c220c82230d940850997a7f84
Saurabh Bathe <sbathe@gmail.com>
2018-03-29 16:45:46 +0530

    minor modifications to convert_filenames_to_lower.sh

--- misc/convert_filenames_to_lower.sh
+++ misc/convert_filenames_to_lower.sh
@@ -8,7 +8,7 @@ if [ -z $1 ];then echo Give target directory; exit 0;fi
 find "$1" -depth -name '*' | while read file ; do
         directory=$(dirname "$file")
         oldfilename=$(basename "$file")
-	newfilename=$(echo "$oldfilename" | tr ' ' '_' | tr -d '[{}(),\!]' | tr -d "\'" | tr '[A-Z]' '[a-z]' | sed 's/_-_/_/g')
+	newfilename=$(echo "$oldfilename" | tr -d '[{}(),\!]' | tr -d "\'\’\&" | tr '[A-Z]' '[a-z]' | tr -s ' ' '_' | sed 's/_-_/_/g')
         if [ "$oldfilename" != "$newfilename" ]; then
                 mv -i "$directory/$oldfilename" "$directory/$newfilename"
                 echo ""$directory/$oldfilename" ---> "$directory/$newfilename""
