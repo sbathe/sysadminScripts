67cd4c95b3a8a9489bef03e65afeebb8afff0928
Saurabh Bathe <sbathe@gmail.com>
2021-02-06 16:19:34 +0530

    added VSCode

--- setup-env.sh
+++ setup-env.sh
@@ -48,3 +48,9 @@ sudo dnf install mplayer mplayer-tools.x86_64
 mkdir -p $HOME/.config/google-chrome
 touch "$HOME/.config/google-chrome/First Run"
 sudo dnf install -y https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+
+# Install VS Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf check-update
+sudo dnf install -y code
