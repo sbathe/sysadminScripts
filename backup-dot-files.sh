99d4a874a4b0c2cfac3c15e7f0a5b51e8b5f3cc0
Saurabh Bathe <sbathe@gmail.com>
2021-02-10 21:59:47 +0530

    added squid files

--- backup-dot-files.sh
+++ backup-dot-files.sh
@@ -2,6 +2,7 @@
 dconf dump /org/gnome/terminal/legacy/profiles:/ > ~/gnome-terminal-profiles.dconf
 p_files=".aliases .bash_profile .bashrc .fzf.bash .profile .vimrc gnome-terminal-profiles.dconf"
 p_dirs="bin .gnupg .ssh"
+squid_files="/etc/squid/squid.conf /etc/squid/squidGuard.conf /etc/cron.daily/squidGuard /etc/profile.d/proxy.sh /etc/dnf/dnf.conf"
 mkdir -p ${HOME}/secrets
 
-tar zcvf ${HOME}/secrets/dot-files.tar.gz -C $HOME --ignore-failed-read $p_files $p_dirs  
+tar zcvf ${HOME}/secrets/dot-files.tar.gz -C $HOME --ignore-failed-read ${p_files} ${p_dirs} ${squid_files} 
