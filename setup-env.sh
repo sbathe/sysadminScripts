f4070e37e2e634a66a298ff401e688c56b3f7721
Saurabh Bathe <sbathe@gmail.com>
2021-02-10 22:00:53 +0530

    add squid

--- setup-env.sh
+++ setup-env.sh
@@ -42,15 +42,5 @@ sudo dnf groupupdate -y sound-and-video
 sudo dnf install -y rpmfusion-free-release-tainted
 sudo dnf install -y libdvdcss
 sudo dnf install -y rpmfusion-nonfree-release-tainted
-sudo dnf install mplayer mplayer-tools.x86_64
 
-#install Google Chrome Browser
-mkdir -p $HOME/.config/google-chrome
-touch "$HOME/.config/google-chrome/First Run"
-sudo dnf install -y https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-
-# Install VS Code
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo dnf check-update
-sudo dnf install -y code
+sudo dnf install squid squidGuard
